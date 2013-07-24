b786fe81b3e269d62789db04237f98e181a34944
Erik Kronberg <erik@variadic.me>
2013-07-24 14:19:48 +0200

    Simple documenting comments

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,23 @@
 angular.module('trainee.controllers', [])
   .controller('RandomQuestionCtrl', ['$scope', 'Questions', function ($scope, Questions) {
 
+    // Grab the random question
+    Questions.get(function (questions) {
+      var randomnum = Math.floor(Math.random()*questions.length);
+      $scope.question = questions[randomnum];
+    });
+
+    // Setup some defaults for answer checking
     $scope.correct = false;
     $scope.attempted = false;
     $scope.red = function () {
       return $scope.attempted && !$scope.correct;
     };
 
-    Questions.get(function (questions) {
-      var randomnum = Math.floor(Math.random()*questions.length);
-      $scope.question = questions[randomnum];
-    });
+    // Determine whether to use radio or checkbox
+    $scope.isMultiple = $scope.question.answer.length > 1;
 
+    // Check if answer is correct on changes to the form
     $scope.checkAnswer = function () {
       console.log("Given: " + $scope.answer.slice(0,1) + ". Real: " + $scope.question.answer[0]);
       if ($scope.question.answer[0] === $scope.answer.slice(0,1))
