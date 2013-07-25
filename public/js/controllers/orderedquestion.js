b955b188418f6f2c2c7c50d4c4156bf5eb61b0f2
Erik Kronberg <erik@variadic.me>
2013-07-25 11:40:38 +0200

    Silence jshint

--- public/js/controllers/orderedquestion.js
+++ public/js/controllers/orderedquestion.js
@@ -1,6 +1,6 @@
 angular.module('trainee.controllers')
   .controller('OrderedQuestionCtrl', ['$scope', '$routeParams', 'Questions', function ($scope, $routeParams, Questions) {
-    $scope.index = parseInt($routeParams["index"]);
+    $scope.index = parseInt($routeParams["index"], 10);
 
     Questions.get(function (questions) {
       $scope.question = questions[$scope.index];
