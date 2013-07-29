0e1de2635e0097356bb0e3424ea2632511824069
Erik Kronberg <erik@variadic.me>
2013-07-29 15:27:08 +0200

    Each service in own file

---
+++ public/js/services/questions.js
@@ -0,0 +1,13 @@
+angular.module('trainee.services')
+  .factory('Questions', ['$http', function ($http) {
+    return {
+      name: 'Question Service',
+      get: function (callback) {
+        // $http.get('data.json').success(function (data) {
+        //   callback(data);
+        // });
+
+        callback(test_questions);
+      }
+    };
+  }]);
