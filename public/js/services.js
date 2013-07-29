ee38a84daacb54622fcc540ee8ecb23dc59e7626
Erik Kronberg <erik@variadic.me>
2013-07-29 11:35:11 +0200

    Score keeping service

--- public/js/services.js
+++ public/js/services.js
@@ -22,4 +22,35 @@ angular.module('trainee.services', [])
         return this.db[key];
       }
     };
+  }])
+  .factory('Score', ['Persistence', function (Persistence) {
+    return {
+      name: "Score Service",
+      kind: "",
+      bind: function (k) { this.kind = k; },
+      generateKey: function (key) {
+        var filtered = _.filter(["Score", this.kind, key],
+                                function (e) { return e; });
+        return filtered.join(":");
+      },
+      correct: function () {
+        var pk = this.generateKey(":correct"),
+            current = Persistence.get(pk) || 0;
+        Persistence.set(pk, current + 1);
+      },
+      incorrect: function () {
+        var pk = this.generateKey(":incorrect"),
+            current = Persistence.get(pk) || 0;
+        Persistence.set(pk, current + 1);
+      },
+      percentage: function () {
+        var pkc = this.generateKey(":correct"),
+            pki = this.generateKey(":incorrect"),
+            correct = Persistence.get(pkc) || 0,
+            incorrect = Persistence.get(pki) || 0,
+            mean = correct / (incorrect + correct) || 0;
+        console.log("Correct: " + correct + "\nIncorrect: " + incorrect + "\nMEAN: " + mean);
+        return Math.floor(mean * 100);
+      }
+    };
   }]);
