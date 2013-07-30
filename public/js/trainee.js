0c1fb0819301596edb7b05fc46d426ca8f65acbb
Erik Kronberg <erik@variadic.me>
2013-07-30 17:28:20 +0200

    Refactor ordered/random question into single

--- public/js/trainee.js
+++ public/js/trainee.js
@@ -1,7 +1,7 @@
 angular.module('trainee', ['trainee.controllers', 'trainee.services'])
   .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/questions/random', {templateUrl: 'partials/randomquestion.html', controller: 'RandomQuestionCtrl'});
-    $routeProvider.when('/questions/ordered/:index', {templateUrl: 'partials/orderedquestion.html', controller: 'OrderedQuestionCtrl'});
+    $routeProvider.when('/questions/random', {templateUrl: 'partials/question.html', controller: 'QuestionCtrl'});
+    $routeProvider.when('/questions/ordered/:index', {templateUrl: 'partials/question.html', controller: 'QuestionCtrl'});
     $routeProvider.when('/examtips/random', {templateUrl: 'partials/examtip.html', controller: 'ExamTipCtrl'});
     $routeProvider.when('/examtips/ordered/:index', {templateUrl: 'partials/examtip.html', controller: 'ExamTipCtrl'});
     $routeProvider.otherwise({redirectTo: '/questions/random'});
